6906f4b92005f21ee8d8cf6cbd173c19b3a74935
syedgouse1005-star <syedgouse1005@gmail.com>
2026-06-03 17:08:50 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,38 @@
+const products = [
+
+{
+  id:1,
+  name:"Wireless Headphones",
+  price:2999,
+  image:"https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+},
+
+{
+  id:2,
+  name:"Smart Watch",
+  price:4999,
+  image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+},
+
+{
+  id:3,
+  name:"Gaming Mouse",
+  price:1499,
+  image:"https://images.unsplash.com/photo-1527814050087-3793815479db"
+},
+
+{
+  id:4,
+  name:"Laptop",
+  price:55999,
+  image:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+},
+
+{
+  id:5,
+  name:"Bluetooth Speaker",
+  price:2499,
+  image:"https://images.unsplash.com/photo-1589003077984-894e133dabab"
+}
+
+];
